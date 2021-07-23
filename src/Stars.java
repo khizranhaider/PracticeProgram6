414d1b88bdd66f5cfab034d2b072b7bc96736758
khizranhaider <khizranhaider229422@gmail.com>
2021-07-24 01:44:51 +0500

    Initial commit

---
+++ src/Stars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Stars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows in Right handed Triangle : ");
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
